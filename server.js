28475d91c98b855a7eec2138e8da0586fac13ad1
sb <sebastian.breuer@picavi.com>
2018-02-08 17:17:19 +0100

    ADD: basic server construct
    server does work in general now. Just need to add methods wich are going to be called via json rpc

---
+++ server.js
@@ -0,0 +1,108 @@
+//==========================================================================
+// Required node-modules
+//==========================================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+//==========================================================================
+// Express / Bodyparser
+//==========================================================================
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+//==========================================================================
+// Error Handling
+//==========================================================================
+app.use(function(err, req, res, next) {
+    console.info("["+new Date+ "] "+"Request  -- " +req.body);
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+      var err = {
+        "code": -32700,
+        "message": "Parse error",
+        "data": err.message,
+        "id": null
+      };
+      console.debug("["+new Date+ "] " + "Err at request: "+ req);
+      console.info("["+new Date+ "] "+"Response  -- " +JSON.stringify(err));
+      res.end(JSON.stringify(err));
+    }
+});
+//==========================================================================
+// Fetching false endpoints
+//==========================================================================
+app.use(function(req, res, next) {
+    
+    if(req.url !== "/api"){
+        res.end();
+    }else{
+        next();
+    }
+});
+//==========================================================================
+// Logging request
+//==========================================================================
+app.use(function(req, res, next) {
+    console.info("["+new Date+ "] "+"Request  -- " +JSON.stringify(req.body));
+    next();
+});
+//==========================================================================
+// Check JSON-RPC-Version
+//==========================================================================
+app.use(function(req, res, next){
+    if(req.body.jsonrpc !== "2.0"){
+        
+        console.debug("["+new Date+ "] " +"wrong version" + req.body.jsonrpc);
+
+        if(req.body.jsonrpc === undefined){
+            var err = {
+                "code": -32600,
+                "message": "Invalid Request",
+                "data": "Json RPC version is undefined.",
+                "id": null
+              };    
+        }
+        else{
+            var err = {
+                "code": -32600,
+                "message": "Invalid Request",
+                "data": "Wrong JSON-RPC version.",
+                "id": null
+              };
+        }
+        console.info("["+new Date+ "] "+"Response  -- " +JSON.stringify(err));
+        res.end(JSON.stringify(err));
+    }
+    else{
+        next();
+    }
+});
+//==========================================================================
+// Does a method exist (in general)?
+//==========================================================================
+app.use(function(req, res, next){
+    if(req.body.method === undefined){
+        var err = {
+            "code": -32600,
+            "message": "Invalid Request",
+            "data": "No method defined.",
+            "id": null
+          };
+          console.info("["+new Date+ "] "+"Response  -- " +JSON.stringify(err));
+          res.end(JSON.stringify(err));
+    }
+    else{
+        next();
+    }
+});
+//==========================================================================
+// Define API route
+//==========================================================================
+app.post('/api', function(req, res){
+    console.info("["+new Date+ "] "+"Response  -- " +JSON.stringify(req.body));
+    res.end(JSON.stringify(req.body));
+});
+//==========================================================================
+// Start listening
+//==========================================================================
+app.listen(8080, function(){
+    console.info("["+new Date+ "] " +"Started server at PORT 8080");
+});
